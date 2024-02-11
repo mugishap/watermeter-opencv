36a35ea23d060bce7c45b908f3eb2a68364f3af8
Precieux Mugisha <precieuxmugisha@gmail.com>
2024-02-11 08:00:45 +0200

    [feat]: Testing script on other images

---
+++ file.py
@@ -0,0 +1,17 @@
+import cv2 
+import pytesseract 
+import numpy as np
+
+pytesseract.pytesseract.tesseract_cmd = r'/usr/local/bin/tesseract' 
+img = cv2.imread('img.png') 
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+blur = cv2.GaussianBlur(gray, (5,5), 0) 
+bin_img = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+
+kernel = np.ones((1, 1), np.uint8) 
+img = cv2.dilate(bin_img, kernel, iterations=1) 
+img = cv2.erode(img, kernel, iterations=1) 
+text = pytesseract.image_to_string(img) 
+
+print(text) 
